e1911184b5197c51fa4d89d1c2ec631c6ee95634
Pulido098 <oscar.pulido03@uptc.edu.co>
2025-02-20 23:02:06 -0500

    Implementacion metodos
    -contains
    -containsAll
    -get
    -remove
    -removeAll
    -Iterator

--- Implementacion_Lista_Interface/src/co/edu/uptc/structures/SimpleList.java
+++ Implementacion_Lista_Interface/src/co/edu/uptc/structures/SimpleList.java
@@ -9,5 +9,101 @@ public class SimpleList<T> implements List<T> {
         this.size = 0;
     }
 
-    
+    @Override
+    public boolean contains(Object o) {
+        for (T item : this) {
+            if (Objects.equals(item, o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object item : c) {
+            if (!contains(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        Node<T> current = head;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node<T> prev = null;
+            for (int i = 0; i < index; i++) {
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
+        }
+        size--;
+        return current.data;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Node<T> current = head, prev = null;
+        while (current != null) {
+            if (Objects.equals(current.data, o)) {
+                if (prev == null) {
+                    head = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                size--;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object item : c) {
+            while (remove(item)) {
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
 }
